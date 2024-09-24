32e4559f812e4cf6d1ccd78c105f4fdfe40236f3
Phạm Đức Toàn <phamductoan220704@gmail.com>
2024-09-24 17:41:41 +0700

    day 16!

--- src/app/(layoutBasic)/songs/page.tsx
+++ src/app/(layoutBasic)/songs/page.tsx
@@ -2,9 +2,21 @@ import { apiBasicClient, apiBasicServer } from "@/app/utils/request";
 import ItemControlCard from "@/component/item-control-card-music";
 import { Grid } from "@mui/material";
 import { Box } from "@mui/system";
+import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 const Songs = async () => {
+  const cookieStore = cookies();
+
+  const refresh_token = cookieStore.get("refresh_token");
+  const access_token = cookieStore.get("access_token");
+
+  if (!access_token) {
+    redirect(`/`);
+  }
+  if (!refresh_token) {
+    redirect(`/login`);
+  }
   const datall: any = await apiBasicServer("GET", "/songs/full");
   const datas = datall?.data || undefined;
   if (!datas && datall.redirect) {
